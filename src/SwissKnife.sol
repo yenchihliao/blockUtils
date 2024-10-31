e3fc2d4b1477c209106506b9faab0b41a3510902
yen <yen@xy.finance>
2024-10-31 10:49:52 +0800

    feat(w3): Add SwissKnife contract

--- src/SwissKnife.sol
+++ src/SwissKnife.sol
@@ -6,12 +6,13 @@ import "@openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin-upgradeable/proxy/utils/Initializable.sol";
 
 contract SwissKnife is Initializable, UUPSUpgradeable, OwnableUpgradeable {
-    function initialize() public initializer {
-        __Ownable_init(msg.sender); // Initialize ownership
+    function initialize(address _owner) public initializer {
+        __Ownable_init(_owner); // Initialize ownership
+        __UUPSUpgradeable_init(); // Initialize UUPS upgradeability
     }
 
-    function delegateForward(address _addr, bytes calldata _calldata) external {
-        (bool success,) = _addr.call(_calldata);
+    function delegateForward(address _addr, bytes calldata _calldata) payable external onlyOwner {
+        (bool success,) = _addr.call{value:msg.value}(_calldata);
         if(!success) {
             revert();
         }
