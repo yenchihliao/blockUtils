cf91d14efa8748e929aa1cb5ecf21537cc16acea
yen <yen@xy.finance>
2024-10-31 10:40:53 +0800

    Add Foundry and Foundry scripts

---
+++ script/DeploySwissKnife.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
+
+import {SwissKnife} from "../src/SwissKnife.sol";
+import {UUPSProxy} from "../src/UUPSProxy.sol";
+
+contract DeploySwissKnife is Script {
+    modifier broadcastAll() {
+        vm.startBroadcast();
+        _;
+        vm.stopBroadcast();
+    }
+    function run() external broadcastAll {
+        // Deploy SwissKnife implementation contract
+        bytes32 _salt = keccak256(abi.encodePacked("SwissKnifeDeploymentSalt"));
+        address _impl = Create2.deploy(0, _salt, type(SwissKnife).creationCode);
+
+        // Deploy the SwissKnife proxy contract using create2
+        // // Calculate a salt for deterministic deployment
+        _salt = keccak256(abi.encodePacked("SwissKnifeProxySalt"));
+        bytes memory _bytecode = abi.encodePacked(
+            type(UUPSProxy).creationCode,
+            abi.encode(
+                _impl,
+                abi.encodeWithSelector(SwissKnife.initialize.selector, msg.sender)
+            )
+        );
+        address _proxy = Create2.deploy(0, _salt, _bytecode);
+
+        // Confirm deployment
+        console.log("SwissKnife logic deployed at:", _impl); // 0x017F8cBFbabf7bA7c38078f7FBF8275D78D9f374
+        console.log("SwissKnife proxy deployed at:", _proxy); // 0x2b223c44C2Ad3f1cFf0022e91c87ad7DbfA05026
+    }
+}
---
+++ script/SendUnsafeCreateRetryableTicket.s.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import {SwissKnife} from "../src/SwissKnife.sol";
+
+abstract contract StubInbox {
+    function unsafeCreateRetryableTicket(
+        address to,
+        uint256 l2CallValue,
+        uint256 maxSubmissionCost,
+        address excessFeeRefundAddress,
+        address callValueRefundAddress,
+        uint256 gasLimit,
+        uint256 maxFeePerGas,
+        bytes calldata data
+    ) virtual external payable returns (uint256);
+
+    function calculateRetryableSubmissionFee(uint256 dataLength, uint256 baseFee)
+    virtual public view returns (uint256);
+
+    function estimateRetryableTicket(
+        address sender,
+        uint256 deposit,
+        address to,
+        uint256 l2CallValue,
+        address excessFeeRefundAddress,
+        address callValueRefundAddress,
+        bytes calldata data
+    ) virtual external;
+
+    event InboxMessageDelivered(uint256 indexed messageNum, bytes data);
+    event RedeemScheduled(bytes32 indexed ticketId, bytes32 indexed retryTxHash, uint64 indexed sequenceNum, uint64 donatedGas, address gasDonor, uint256 maxRefund, uint256 submissionFeeRefund);
+}
+// Usage: forg script --rpc-url $L1_URL --private-key $KEY "script/SendUnsafeCreateRetryableTicket.s.sol:SendUnsafeCreateRetryableTicket"
+contract SendUnsafeCreateRetryableTicket is Test, Script {
+    modifier broadcastAll() {
+        vm.startBroadcast();
+        _;
+        vm.stopBroadcast();
+    }
+    function run() external {
+        // StubInbox _inbox__ = StubInbox(0xaAe29B0366299461418F5324a79Afc425BE5ae21); // sepolia
+        StubInbox _inbox__ = StubInbox(0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f); // Eth
+
+        // SwissKnife _swissKnife__ = SwissKnife(0x2b223c44C2Ad3f1cFf0022e91c87ad7DbfA05026); // arb sepolia
+        SwissKnife _swissKnife__ = SwissKnife(0x78046053E5B02Ca2056D7007cf55747244223E5B); // Eth
+        address _receiver = 0x51c289a2C7aE30BC39D60F0d210cC17FA15C8950;
+        address _receiver2 = 0x856c363e043Ac34B19D584D3930bfa615947994E;
+        address _receiver3 = 0x9C9F55ebc51D0D606227790d14Afcb706178dE98;
+        uint256 _bridgeAmount = 1 wei;
+
+        // Get the maxSubmissionCost on L1
+        uint256 _maxSubmissionCost = _inbox__.calculateRetryableSubmissionFee(0, 0);
+        require(_maxSubmissionCost == block.basefee * 1400); // L1 block.basefee * 1400
+        _maxSubmissionCost *= 2;
+
+        // Get the gasLimit on L2
+        // Transfer only takes 21000 gas
+        uint256 _gasLimit = 21000;
+
+        // Get the maxFeePerGas on L2
+        // tx.gasprice
+        // $cast rpc -r $URL eth_gasPrice
+        uint256 _maxFeePerGas = 1e7;
+        _maxFeePerGas *= 2;
+
+        bytes memory _calldata = abi.encodeWithSelector(
+            _inbox__.unsafeCreateRetryableTicket.selector,
+            _receiver,
+            _bridgeAmount,
+            _maxSubmissionCost,
+            _receiver2,
+            _receiver3,
+            _gasLimit,
+            _maxFeePerGas,
+            ""
+        );
+
+        uint256 _inputAmount = _bridgeAmount + _maxSubmissionCost + _maxFeePerGas * _gasLimit;
+        console.log("Input amount: %d", _inputAmount);
+        vm.startBroadcast();
+        _swissKnife__.delegateForward{value: _inputAmount}(address(_inbox__), _calldata);
+        // address(_inbox__).call{value: _inputAmount}(_calldata);
+        vm.stopBroadcast();
+    }
+}
